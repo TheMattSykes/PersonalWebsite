4bf9a2e65b0e6d5c38255480d912ac728e8f279a
TheMattSykes <mtsykes1@sheffield.ac.uk>
2021-04-24 16:08:06 +0100

    Build of previous commit.

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Volumes/Matt-SSD-Store/Projects/personal-website/.cache/dev-404-page.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Volumes/Matt-SSD-Store/Projects/personal-website/src/pages/about.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/Volumes/Matt-SSD-Store/Projects/personal-website/src/pages/contact.js"))),
   "component---src-pages-first-year-js": hot(preferDefault(require("/Volumes/Matt-SSD-Store/Projects/personal-website/src/pages/first-year.js"))),
